315e0c1729868988c43e317165f17cdf455ce72b
ConShaunery <90924398+ConShaunery@users.noreply.github.com>
2026-01-16 07:30:47 -0500

    Update guestbook.js

--- netlify/functions/guestbook.js
+++ netlify/functions/guestbook.js
@@ -1,6 +1,23 @@
-exports.handler = async function () {
+let entries = [];
+
+exports.handler = async function (event) {
+  if (event.httpMethod === "POST") {
+    const data = JSON.parse(event.body);
+
+    entries.unshift({
+      name: data.name,
+      message: data.message,
+      date: new Date().toISOString()
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ ok: true })
+    };
+  }
+
   return {
     statusCode: 200,
-    body: "FUNCTION WORKS"
+    body: JSON.stringify(entries)
   };
 };
